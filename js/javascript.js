4fbe7daebc685ad8f12676406d21f9e363cff304
GPTEnvironment <lucas@acriar.com.br>
2025-09-17 12:40:16 -0300

    mudanças

--- js/javascript.js
+++ js/javascript.js
@@ -5,9 +5,24 @@ let luz = document.querySelector(".back-luz")
 let botao = document.querySelector(".botao")
 const lista = ["back-azul","back-vermelho","back-abismo", "back-palido","back-luz", "back-zote"]
 let index = 0;
+  var audio = new Audio("js/hollow.mp3");
+  audio.loop = true
+
+function isplaying(value) {
+   return !value.paused;
+}
 
 botao.addEventListener("click", () => {
   const next = (index + 1) % lista.length;
   shaw.classList.replace(lista[index], lista[next]);
   index = next;
+
+    if(isplaying){
+        audio.play();
+    }
+    
+      
+      
+
+  
 });
